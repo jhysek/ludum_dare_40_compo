8059cfe405e1ec378c3362f24c8fa67deed706fa
Jiri Hysek <kontakt@jirihysek.cz>
2017-12-04 02:29:25 +0100

    Submitted version

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://Scenes/Level1.tscn")
+	get_tree().change_scene("res://Scenes/" + GameState.get_current_level_name() + ".tscn")
 	
 func _on_Button1_pressed():
 	get_tree().quit()
